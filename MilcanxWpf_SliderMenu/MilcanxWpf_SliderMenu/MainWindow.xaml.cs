c3f0f2a53bddec22c03ad08e19ea04fd4bab8aa1
anhho88 <chienthan18111988@outlook.com>
2018-01-13 20:11:56 +0700

    new

---
+++ MilcanxWpf_SliderMenu/MilcanxWpf_SliderMenu/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MilcanxWpf_SliderMenu
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        //private void myButton_Click(object sender, RoutedEventArgs e)
+        //{
+        //    ShowHideMenu("sbShowLeftMenu", myButton2, myButton, spLeft);
+        //}
+
+        private void ShowHideMenu(string storyboardhide, Button myButton, Button myButton2, StackPanel pnl)
+        {
+            Storyboard sb = Resources[storyboardhide] as Storyboard;
+            sb.Begin(pnl);
+
+            if (storyboardhide.Contains("Show"))
+            {
+                myButton.Visibility = System.Windows.Visibility.Visible;
+                myButton2.Visibility = System.Windows.Visibility.Hidden;
+            }
+            else if (storyboardhide.Contains("Hide"))
+            {
+                myButton.Visibility = System.Windows.Visibility.Hidden;
+                myButton2.Visibility = System.Windows.Visibility.Visible;
+            }
+
+
+        }
+
+        //private void myButton2_Click(object sender, RoutedEventArgs e)
+        //{
+        //    ShowHideMenu("sbHideLeftMenu", myButton2, myButton, spLeft);
+        //}
+    }
+}
---
+++ MilcanxWpf_SliderMenu/MilcanxWpf_SliderMenu/MesajGetir.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+
+namespace MilcanxWpf_SliderMenu
+{
+    public class MesajGetir
+    {
+
+        //public static bool GetSelectAll(TextBoxBase target)
+        //{
+        //    return (bool)target.GetValue(SelectAllAttachedProperty);
+        //}
+
+        //public static void SetSelectAll(TextBoxBase target, bool value)
+        //{
+        //    target.SetValue(SelectAllAttachedProperty, value);
+        //}
+
+        //public static readonly DependencyProperty SelectAllAttachedProperty = DependencyProperty.RegisterAttached("SelectAll", typeof(bool), typeof(MesajGetir), new UIPropertyMetadata(false, OnSelectAllAttachedPropertyChanged));
+
+        //static void OnSelectAllAttachedPropertyChanged(DependencyObject o, DependencyPropertyChangedEventArgs e)
+        //{
+        //    ((TextBoxBase)o).SelectAll();
+        //}
+
+
+    }
+}
