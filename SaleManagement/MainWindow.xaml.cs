f8df111c982efb7e2c94d96ddfb8f201aff08e36
anhho88 <chienthan18111988@outlook.com>
2018-01-29 01:52:33 +0700

    29/01/2018

---
+++ SaleManagement/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SaleManagement {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+
+        void _configStartUpLayout() {
+            this.Top = 0;
+            this.Left = 0;
+            this.Width = System.Windows.SystemParameters.WorkArea.Width * 0.85;
+            this.Height = System.Windows.SystemParameters.WorkArea.Height * 0.85;
+        }
+
+        public MainWindow() {
+            InitializeComponent();
+            _configStartUpLayout();
+        }
+
+        private void closeApplication_MouseDown(object sender, MouseButtonEventArgs e) {
+            Application.Current.Shutdown();
+        }
+
+        private void Border_MouseDown(object sender, MouseButtonEventArgs e) {
+            try {
+                this.DragMove();
+            } catch { }
+        }
+    }
+}
